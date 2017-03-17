cd4cb7a887d9c9fdfcf5907dbed6dcfa4eb2ede8
Robert Brice <rob.a.brice@gmail.comC>
2017-03-17 16:17:57 +0000

    layout and class for create new task cleanup

--- app/src/main/java/com/codeclan/todolist/CreateNewTaskActivity.java
+++ app/src/main/java/com/codeclan/todolist/CreateNewTaskActivity.java
@@ -1,13 +1,41 @@
 package com.codeclan.todolist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateNewTaskActivity extends AppCompatActivity {
+
+    EditText taskName;
+    EditText taskDescription;
+    EditText taskCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_task);
+
+        taskName = (EditText)findViewById(R.id.input_name);
+        taskDescription = (EditText)findViewById(R.id.input_description);
+        taskCategory = (EditText)findViewById(R.id.input_category);
     }
+
+    public void onCreateButtonClicked(View button){
+
+        Intent intent = new Intent(this, ListActivity.class);
+
+        String name = taskName.getText().toString();
+        String description = taskDescription.getText().toString();
+        String category = taskCategory.getText().toString();
+
+        //TO DO - got to actually save the new task...
+
+        Toast.makeText(CreateNewTaskActivity.this, "Task saved", Toast.LENGTH_SHORT).show();
+
+        startActivity(intent);
+    }
+
 }
