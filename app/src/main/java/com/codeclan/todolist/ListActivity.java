11f4173771f378bd6a4edfae6e8bbc881f231198
Robert Brice <rob.a.brice@gmail.comC>
2017-03-17 12:54:06 +0000

    add ListAdapter with done/not done flags

--- app/src/main/java/com/codeclan/todolist/ListActivity.java
+++ app/src/main/java/com/codeclan/todolist/ListActivity.java
@@ -3,11 +3,11 @@ package com.codeclan.todolist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.task_list);
     }
 }
---
+++ app/src/main/java/com/codeclan/todolist/ListAdapter.java
@@ -0,0 +1,48 @@
+package com.codeclan.todolist;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ListAdapter extends ArrayAdapter<Task> {
+
+    public ListAdapter(Context context, ArrayList<Task> tasks){
+        super(context, 0, tasks);
+    }
+
+    public View getView(int position, View listItemView, ViewGroup parent){
+
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.task_item,
+                    parent,
+                    false);
+        }
+
+        Task currentTask = getItem(position);
+
+        ImageView completionStatus (ImageView)listItemView.findViewById(R.id.image);
+
+        if (currentTask.isDone()) {
+            completionStatus.setImageResource(currentTask.getDoneFlag());
+        }
+        else
+        {
+            completionStatus.setImageResource(currentTask.getNotDoneFlag());
+        }
+
+        TextView name = (TextView)listItemView.findViewById(R.id.name);
+        name.setText(currentTask.getName().toString());
+
+
+
+    }
+
+}
