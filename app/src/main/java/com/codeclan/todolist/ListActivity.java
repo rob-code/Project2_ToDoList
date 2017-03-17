e58b62ccb8fe1efb209ca42846c9c9baa55bf633
Robert Brice <rob.a.brice@gmail.comC>
2017-03-17 17:22:06 +0000

    added 1st draft add task button to App Bar

--- app/src/main/java/com/codeclan/todolist/ListActivity.java
+++ app/src/main/java/com/codeclan/todolist/ListActivity.java
@@ -3,7 +3,11 @@ package com.codeclan.todolist;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -67,6 +71,45 @@ public class ListActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+
+    //working with the add button - but it needs not to be an options menu!
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.add_new_task:
+                onAddTaskClicked();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onAddTaskClicked(){
+
+        //RelativeLayout title = (RelativeLayout) textView;
+        Intent intent = new Intent(this, CreateNewTaskActivity.class);
+        //intent.putExtra("task name", title.getText().toString());
+
+        //just testing out for the meantime
+        intent.putExtra("name", "task name stuff goes here");
+        intent.putExtra("description", "task description stuff goes here");
+        intent.putExtra("category", "task category stuff goes here");
+
+        startActivity(intent);
+
+
+
+
+    }
 
 
 }
