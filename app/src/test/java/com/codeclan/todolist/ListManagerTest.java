cb432f27ad251b86956a1e02d2c794feeefc7762
Robert Brice <rob.a.brice@gmail.comC>
2017-03-19 10:47:23 +0000

    changed test file name to correspond to ListManager refactor

--- app/src/test/java/com/codeclan/todolist/ListManagerTest.java
+++ app/src/test/java/com/codeclan/todolist/ListManagerTest.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 
-public class TaskListTest {
+public class ListManagerTest {
 
     ListManager listManager;
     ArrayList<Task> testList;
